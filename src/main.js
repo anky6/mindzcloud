34475be2e531b2b4931426fad99ba25bb14d45c7
Spartan <ankitcool583@gmail.com>
2018-02-06 19:06:38 +0530

    mongodb configuration

--- src/main.js
+++ src/main.js
@@ -12,8 +12,18 @@ import './assets/css/style.css'
 
 import {store} from './store'
 
+import { StitchClientFactory } from 'mongodb-stitch'
+let appId = 'myapp-lotbo'
+let stitchClientPromise = StitchClientFactory.create(appId)
+
 Vue.config.productionTip = false
 Vue.use(Vuetify)
+stitchClientPromise.then(client => {
+  let db = client.service('mongodb', 'mongodb-atlas').db('Mindzcloud')
+  let item = db.collection('Users')
+  console.log(item)
+}).then(result => console.log('success: ', result))
+  .catch(e => console.log('error: ', e))
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
